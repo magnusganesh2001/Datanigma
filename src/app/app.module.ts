00e5aadec6b841863367fc38ecb06e49c804dfa2
JerubSinghSamuel <jerubsingh@gmail.com>
2022-05-19 17:27:15 +0530

    added job page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,14 +8,18 @@ import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 import { HomeComponent } from './home/home.component';
 import { ReactiveFormsModule } from '@angular/forms';
-import { MatButtonModule } from '@angular/material/button'
-
+import { MatButtonModule } from '@angular/material/button';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FindJobsComponent } from './find-jobs/find-jobs.component'
+import { MatCardModule } from '@angular/material/card';
+import { MatTableModule } from '@angular/material/table'
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     SignupComponent,
     HomeComponent,
+    FindJobsComponent,
   ],
   imports: [
     BrowserModule,
@@ -23,6 +27,9 @@ import { MatButtonModule } from '@angular/material/button'
     ReactiveFormsModule,
     HttpClientModule,
     MatButtonModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatTableModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
